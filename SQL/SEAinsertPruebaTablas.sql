76d10a7ef4a49f322f11fbfd87268961c90ab877
John Delgado <johndelgado@MacBook-Pro-de-John.local>
2013-09-30 21:48:28 -0430

    Cargados los archivos sql asociados a la base de datos

---
+++ SQL/SEAinsertPruebaTablas.sql
@@ -0,0 +1,77 @@
+INSERT INTO TIPO_USUARIO VALUES ('Administrador');
+
+INSERT INTO USUARIO VALUES
+    ('usb', 'usb', 'Simon', 'Bolivar', 'cosa@usb.ve', 'Administrador');
+
+INSERT INTO INSTITUCION VALUES
+    ('USB001', 'Universidad Simon Bolivar', 'Sartenejas', 'Venezuela'),
+    ('UCAB001', 'Universidad Catolica Andres Bello', 'Montalban', 'Venezuela'),
+    ('UCV001', 'Universidad Central de Venezuela', 'Plaza Venezuela', 'Venezuela');
+    
+INSERT INTO CARRERA VALUES
+    ('0100', 'USB001', 'Ingenieria Electrica'),
+    ('0200', 'USB001', 'Ingenieria Produccion'),
+    ('0300', 'USB001', 'Ingenieria Materiales'),
+    ('0800', 'USB001', 'Ingenieria Computacion'),
+    ('3838', 'UCAB001', 'Ingenieria Mecanica'),
+    ('2727', 'UCAB001', 'Ingenieria Informatica');
+
+
+INSERT INTO ASIGNATURA VALUES
+    ('USB001', 'MA1111', 'Matematicas I', 4),
+    ('USB001', 'MA2222', 'Matematicas II',4),
+    ('USB001', 'MA3333', 'Matematicas III',4),
+    ('USB001', 'MA4444', 'Matematicas IV',4),
+    ('USB001', 'MA5555', 'Matematicas V',4),
+    ('USB001', 'MA6666', 'Matematicas VI',4),
+    ('USB001', 'MA7777', 'Matematicas VII',4),
+    ('USB001', 'LA1111', 'Lenguaje I',3),
+    ('USB001', 'LA2222', 'Lenguaje II',3),
+    ('USB001', 'LA3333', 'Lenguaje III',3),
+    ('USB001', 'CS1111', 'Sociales I',3),
+    ('USB001', 'CS2222', 'Sociales II',3),
+    ('USB001', 'CS3333', 'Sociales III',3),
+    ('USB001', 'FS1111', 'Fisica I',3),
+    ('USB001', 'FS2222', 'Fisica II',3),
+    ('USB001', 'FS3333', 'Fisica III',3),
+    ('UCAB001', 'MAT001', 'Calculo I',2),
+    ('UCAB001', 'MAT002', 'Calculo II',2),
+    ('UCAB001', 'MAT003', 'Calculo III',3),
+    ('UCAB001', 'MAT004', 'Calculo IV',4),
+    ('UCAB001', 'LEN001', 'Lenguaje y Literatura I',3),
+    ('UCAB001', 'LEN002', 'Lenguaje y Literatura II',2),
+    ('UCAB001', 'LEN003', 'Lenguaje y Literatura III',3),
+    ('UCAB001', 'FIS001', 'Fisica I',2),
+    ('UCAB001', 'FIS002', 'Fisica II',4),
+    ('UCAB001', 'FIS003', 'Fisica III',4);
+    
+
+INSERT INTO TABLA_EQUIVALENCIA VALUES
+    ('USB001', '0800', 'UCAB001', '2727', CURRENT_DATE),
+    ('UCAB001', '2727', 'USB001', '0800', CURRENT_DATE),
+    ('USB001', '0800', 'UCAB001', '3838', CURRENT_DATE);
+
+
+INSERT INTO EQUIVALENCIA VALUES
+    ('USB001', '0800', 'UCAB001', '2727', '1'),
+    ('USB001', '0800', 'UCAB001', '2727', '2'),
+    ('USB001', '0800', 'UCAB001', '2727', '3'),
+    ('USB001', '0800', 'UCAB001', '2727', '4');
+
+INSERT INTO INCLUYE VALUES
+    ('USB001', '0800', 'MA1111', 'UCAB001', '2727', '1'),
+    ('USB001', '0800', 'LA1111', 'UCAB001', '2727', '2'),
+    ('USB001', '0800', 'FS1111', 'UCAB001', '2727', '3'),
+    ('USB001', '0800', 'FS2222', 'UCAB001', '2727', '4'),
+    ('USB001', '0800', 'FS3333', 'UCAB001', '2727', '4');
+
+INSERT INTO EQUIVALE VALUES
+    ('USB001', '0800', 'UCAB001', '2727', 'MAT001', '1'),
+    ('USB001', '0800', 'UCAB001', '2727', 'MAT002', '1'),
+    ('USB001', '0800', 'UCAB001', '2727', 'LEN001', '2'),
+    ('USB001', '0800', 'UCAB001', '2727', 'FIS001', '3'),
+    ('USB001', '0800', 'UCAB001', '2727', 'FIS002', '4');
+
+
+
+
